5d3f2f61513dd8c1244c64914489284c507b0884
Josh Z <jzieger10@gmail.com>
2019-01-15 15:12:13 -0500

    Added endpoints for zoos

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const db = knex(knexConfig.development);
 // endpoints here
+
+server.get("/", (req, res) => {
+	res.send("API working");
+});
+
+
+server.get("/api/zoos", (req, res) => {
+	db("zoos")
+		.then(zoos => {
+			res.status(200).json(zoos);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id})
+		.then(zoo => {
+			res.status(200).json(zoo);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+
+server.post("/api/zoos", (req, res) => {
+	db("zoos")
+		.insert(req.body)
+		.then(ids => {
+			res.status(201).json(ids);
+		})
+		.catch(err => {
+			res.status(500).json({error: "There has been a server error on the POST route"});
+		});
+});
+
+
+server.delete("/api/zoos/:id", (req, res) => {
+	db("zoos")
+		.where({ id: req.params.id })
+		.del()
+		.then(count => {
+			res.status(201).json(count);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
+server.put("/api/zoos/:id", (req, res) => {
+	db("zoos")
+        .where({ id: req.params.id })
+        .update(req.body)
+		.then(count => {
+			res.status(201).json(count);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
